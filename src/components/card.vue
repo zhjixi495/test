a0f7569b3f3f5521bd91097d8c1e6abbdec28cdb
Caoanlong <caoanlong@gmail.com>
2018-05-22 15:38:49 +0800

    ggg

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,19 +1,56 @@
 <template>
-  <div>
-    <p class="card">
-      {{text}}
-    </p>
-  </div>
+	<view class='li'>
+		<text class='txt'>设备{{index + 1}}：{{device.name ? device.name : device.deviceId}}</text>
+		<text class='connecting' v-if="isConnectingDevice == device.deviceId">正在配对...</text>
+		<button size='mini' type="primary" class='connect' @click='startConnectDevices()' v-else>连接</button>
+	</view>
 </template>
 
 <script>
 export default {
-  props: ['text']
+	props: {
+		device: {
+			type: Object
+		},
+		index: {
+			type: Number
+		},
+		isConnectingDevice: {
+			type: String,
+			default: ''
+		}
+	},
+	data() {
+		return {
+			isConnecting: ''
+		}
+	},
+	methods: {
+		startConnectDevices() {
+			if (this.isConnectingDevice) return
+			this.$emit('startConnectDevices', this.device.deviceId)
+		}
+	}
 }
 </script>
 
-<style>
-.card {
-  padding: 10px;
-}
+<style lang="stylus" scoped>
+.li
+	display flex
+	height 50px
+	line-height 50px
+	border-bottom 1px solid #ddd
+	align-items center
+	&:last-child
+		border-bottom none
+	.txt
+		flex 1
+	.connect
+		flex 0 0 60px
+		height 30px
+		text-align right
+	.connecting
+		flex 0 0 80px
+		text-align right
+		color #ccc
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,18 @@
 <template>
 	<div class="container">
-		<view class='btn-group'>
+		<!-- <view class='btn-group'>
 			<button size='mini' @click="startConnect()">开始搜索</button>
 			<button size='mini' @click="stopBluetoothDevicesDiscovery()">停止搜索</button>
-		</view>
+		</view> -->
 		<view class='list' v-if="devices.length > 0">
-			<view class='li' v-for="(device, index) in devices">
-				<text class='txt'>设备{{index + 1}}：{{device.name}}</text>
-				<text class='connect' @click='startConnectDevices(device.deviceId)'>连接</text>
-			</view>
+			<card 
+				v-for="(device, index) in devices" 
+				:key="device.deviceId" 
+				:device="device" 
+				:index="index" 
+				:isConnectingDevice="isConnectingDevice" 
+				@startConnectDevices="startConnectDevices">
+			</card>
 		</view>
 	</div>
 </template>
@@ -20,11 +24,14 @@ export default {
 	data () {
 		return {
 			userInfo: {},
-			devices: []
+			devices: [],
+			isConnectingDevice: '',
+			serviceId: ''
 		}
 	},
 	created () {
 		// 调用应用实例的方法获取全局数据
+		this.startConnect()
 	},
 	methods: {
 		// 开始
@@ -44,15 +51,14 @@ export default {
 						title: '蓝牙初始化失败',
 						duration: 2000
 					})
-					setTimeout(function () {
+					setTimeout(() => {
 						wx.hideToast()
 					}, 2000)
 				}
 			})
 			wx.onBluetoothAdapterStateChange(res => {
 				console.log(res)
-				let available = res.available
-				if (available) {
+				if (res.available) {
 					this.getBluetoothAdapterState()
 				}
 			})
@@ -61,20 +67,17 @@ export default {
 		getBluetoothAdapterState() {
 			wx.getBluetoothAdapterState({
 				success: res => {
-					let available = res.available
-					let discovering = res.discovering
-					if (!available) {
+					if (!res.available) {
 						wx.showToast({
 							title: '设备无法开启蓝牙连接',
 							duration: 2000
 						})
-						setTimeout(function () {
+						setTimeout(() => {
 							wx.hideToast()
 						}, 2000)
 					} else {
-						if (!discovering) {
+						if (!res.discovering) {
 							this.startBluetoothdeviceDiscovery()
-							this.getConnectedBluetoothDevices()
 						}
 					}
 				}
@@ -101,55 +104,11 @@ export default {
 				}
 			})
 		},
-		// 获取已配对的蓝牙设备
-		getConnectedBluetoothDevices() {
-			wx.getConnectedBluetoothDevices({
-				// services: [that.serviceId],
-				services: [],
-				success: res => {
-					console.log("获取处于连接状态的设备", res['devices'])
-					this.devices = Array.from(new Set(this.devices.concat(res['devices'])))
-					if (this.devices.length > 0) {
-						wx.hideLoading()
-					}
-					let devices = res['devices'], flag = false, index = 0, conDevList = []
-					console.log(devices)
-					devices.forEach((value, index, array) => {
-						if (value['name'].indexOf('FeiZhi') != -1) {
-							// 如果存在包含FeiZhi字段的设备
-							flag = true
-							index += 1
-							conDevList.push(value['deviceId'])
-							that.deviceId = value['deviceId']
-							return
-						}
-					})
-					if (flag) {
-						this.connectDeviceIndex = 0
-						this.loopConnect(conDevList)
-					} else {
-						if (!this.getConnectedTimer) {
-							this.getConnectedTimer = setTimeout(() => {
-								this.getConnectedBluetoothDevices()
-							}, 5000)
-						}
-					}
-				},
-				fail: err => {
-					if (!this.getConnectedTimer) {
-						this.getConnectedTimer = setTimeout(() => {
-							this.getConnectedBluetoothDevices()
-						}, 5000)
-					}
-				}
-			})
-		},
-		// 开启蓝牙搜索功能失败
+		// 监听寻找到新设备的事件
 		onBluetoothDeviceFound() {
 			console.log('onBluetoothDeviceFound')
 			wx.onBluetoothDeviceFound(res => {
-				console.log('new device list has founded')
-				console.log(res['devices'])
+				console.log('发现新的设备', res['devices'])
 				this.devices = Array.from(new Set(this.devices.concat(res['devices'])))
 				if (this.devices.length > 0) {
 					wx.hideLoading()
@@ -158,62 +117,90 @@ export default {
 		},
 		// 开始连接要配对的设备
 		startConnectDevices(deviceId) {
-			clearTimeout(this.getConnectedTimer)
-			this.getConnectedTimer = null
-			// clearTimeout(this.discoveryDevicesTimer)
 			this.stopBluetoothDevicesDiscovery()
-			this.isConnectting = true
+			this.isConnectingDevice = deviceId
 			wx.createBLEConnection({
 				deviceId: deviceId,
 				success: res => {
+					console.log('开始连接要配对的设备', res)
 					if (res.errCode == 0) {
 						setTimeout(() => {
+							this.isConnectingDevice = ''
 							this.getService(deviceId)
-						}, 5000)
+						}, 3000)
 					}
 				},
-				fail: function (err) {
-					console.log('连接失败：', err)
+				fail: err => {
+					this.isConnectingDevice = ''
+					console.log('配对失败：', err)
+					wx.showToast({
+						title: '配对失败',
+						duration: 2000
+					})
+					setTimeout(function () {
+						wx.hideToast()
+					}, 2000)
 				},
 				complete: function () {
 					console.log('complete connect devices')
-					this.isConnectting = false
 				}
 			})
 		},
 		// 连接成功后根据deiviceId获取设备的所有服务
 		getService(deviceId) {
-			// 监听蓝牙连接
+			// 监听蓝牙连接(重连)
 			wx.onBLEConnectionStateChange(res => {
-				console.log(res)
+				this.isConnectingDevice = ''
+				console.log(`device ${res.deviceId} state has changed, connected: ${res.connected}`)
+				wx.showToast({
+					title: '已断开',
+					duration: 2000
+				})
+				setTimeout(() => {
+					wx.hideToast()
+				}, 2000)
 			});
 			// 获取蓝牙设备service值
 			wx.getBLEDeviceServices({
 				deviceId: deviceId,
 				success: res => {
+					console.log('获取蓝牙设备service值', res)
 					this.getCharacter(deviceId, res.services)
 				}
 			})
 		},
 		// 读取服务的特征值
 		getCharacter(deviceId, services) {
-			services.forEach((value, index, array) => {
-				if (value == this.serviceId) {
-					this.serviceId = array[index]
-				}
-			})
+			this.serviceId = services[0].uuid
 			wx.getBLEDeviceCharacteristics({
 				deviceId: deviceId,
 				serviceId: this.serviceId,
-				success: (res) => {
-					this.writeBLECharacteristicValue(deviceId, this.serviceId, this.characterId_write)
-					this.openNotifyService(deviceId, this.serviceId, this.characterId_read)
+				success: res => {
+					console.log('获取蓝牙设备特征值', res)
+					let characteristicIdRead = ''
+					if (res['characteristics'][0].properties.read) {
+						characteristicIdRead = res['characteristics'][0].uuid
+					}
+					this.readBLECharacteristicValue(deviceId, this.serviceId, characteristicIdRead)
+					// this.writeBLECharacteristicValue(deviceId, this.serviceId, this.characterId_write)
+					// this.openNotifyService(deviceId, this.serviceId, this.characterId_read)
 				},
-				fail: (err) => {
+				fail: err => {
 					console.log(err)
 				},
 				complete: () => {
 					console.log('complete')
+				}
+			})
+		},
+		// 
+		readBLECharacteristicValue(deviceId, serviceId, characteristicId) {
+			wx.readBLECharacteristicValue({
+				deviceId: deviceId, 
+				serviceId: serviceId, 
+				characteristicId: characteristicId,
+				success: res => {
+					console.log('readBLECharacteristicValue:', res)
 				}
 			})
 		},
@@ -233,37 +220,17 @@ export default {
 }
 </script>
 
-<style scoped>
-.btn-group {
-	display: flex;
-	width: 100%
-}
-.btn-group button {
-	width: 120px
-}
-.list {
-	width: 80%;
-	margin: 20px auto;
-	border: 1px solid #ddd;
-	padding: 0 10px;
-	font-size: 14px;
-	background-color: #f6f6f6
-}
-.list .li {
-	display: flex;
-	height: 50px;
-	line-height: 50px;
-	border-bottom: 1px solid #ddd;
-}
-.list .li:last-child {
-	border-bottom: none
-}
-.list .li .txt {
-	flex: 1
-}
-.list .li .connect {
-	flex: 0 0 60px;
-	text-align: right;
-	color: blue;
-}
+<style lang="stylus" scoped>
+.btn-group
+	display flex
+	width 100%
+	button
+		width 120px
+.list
+	width 80%
+	margin 20px auto
+	border 1px solid #ddd
+	padding 0 10px
+	font-size 14px
+	background-color #f6f6f6
 </style>
